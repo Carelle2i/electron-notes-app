57e07f36b5f2a213e7918bb4c74f00aac426422e
Carelle2i <158145969+Carelle2i@users.noreply.github.com>
2025-05-07 15:43:45 +0200

    update

--- cypress/e2e/integration/notes.cy.js
+++ cypress/e2e/integration/notes.cy.js
@@ -1,26 +1,40 @@
-describe('Electron Notes App', () => {
+describe('Renderer Test for Electron Notes App', () => {
 
-  it('should load the main window and display existing notes', () => {
+  beforeEach(() => {
     cy.visit('index.html');
+  });
+
+  it('should load the main window and display UI elements', () => {
     cy.get('#note-input').should('be.visible');
     cy.get('#save-button').should('be.visible');
     cy.get('#notes-list').should('be.visible');
   });
 
-  it('should add a new note', () => {
+  it('should add a new note and display it in the list', () => {
     const newNote = 'Test Note';
+
     cy.get('#note-input').type(newNote);
     cy.get('#save-button').click();
-    cy.get('.note-item').should('contain', newNote);
+
+    cy.get('.note-item').last().should('contain', newNote);
   });
 
-  it('should delete a note', () => {
-    const supprimer = 'Note to Delete';
-    cy.get('#note-input').type(supprimer);
+  it('should delete a note and remove it from the list', () => {
+    const noteToDelete = 'Note to Delete';
+    
+    cy.get('#note-input').type(noteToDelete);
     cy.get('#save-button').click();
+
     cy.get('.delete-button').last().click();
-    cy.get('.note-item').should('not.contain', supprimer);
+
+    cy.get('.note-item').should('not.contain', noteToDelete);
   });
 
+  it('should not add an empty note', () => {
+    cy.get('#note-input').type('  ');
+    cy.get('#save-button').click();
+    
+    cy.get('.note-item').should('not.exist');
+  });
 
 });
